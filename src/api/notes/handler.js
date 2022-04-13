0c72a01f7c8b8e4a6d93fdbc54889f8e5392c90f
Goki Purba <goki.purba@gmail.com>
2022-04-13 22:42:46 +0700

    Fix bug

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -1,4 +1,4 @@
-const ClientError = require("../../exceptions/clienterror");
+const ClientError = require("../../exceptions/ClientError");
 
 class NotesHandler {
     constructor(service, validator) {
--- src/exceptions/InvariantError.js
+++ src/exceptions/InvariantError.js
@@ -1,4 +1,4 @@
-const ClientError = require("./clienterror");
+const ClientError = require("./ClientError");
 
 class InvariantError extends ClientError {
     constructor (message) {

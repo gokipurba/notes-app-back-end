a7a2e29e8845db7e9803be3e78cec3e3c7a7411d
Goki Purba <goki.purba@gmail.com>
2022-04-17 15:41:09 +0700

    user registration implemented

--- src/server.js
+++ src/server.js
@@ -1,10 +1,14 @@
 require('dotenv').config() // kini nilai dari variable environment yang ada di berkas .env dapat diakses melalui properti process.env.
 const Hapi = require('@hapi/hapi');
 const notes = require('./api/notes');
+const users = require('./api/users');
 const NotesService = require('./services/postgres/NotesService');
+const UsersService = require('./services/postgres/UsersService');
 const NotesValidator = require('./validator/notes');
+const UsersValidator = require('./validator/users');
 
 const init = async () => {
+  const userService = new UsersService;
   const notesService = new NotesService();
   const server = Hapi.server({
     port: process.env.PORT,
@@ -16,13 +20,24 @@ const init = async () => {
     },
   });
 
-  await server.register({
-    plugin: notes,
-    options: {
-      service: notesService,
-      validator: NotesValidator,  
+  
+  await server.register([
+    {
+      plugin: notes,
+      options: {
+        service: notesService,
+        validator: NotesValidator,  
+      },
     },
-  });
+    {
+      plugin: users,
+      options: {
+        service: userService,
+        validator: UsersValidator,  
+      },
+    }
+
+]);
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);

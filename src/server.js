34cc4e065608b7f9626b3807407c80ce78e0d195
Goki Purba <goki.purba@gmail.com>
2022-04-19 13:17:52 +0700

    solved Typo in server

--- src/server.js
+++ src/server.js
@@ -1,21 +1,30 @@
-require('dotenv').config() // kini nilai dari variable environment yang ada di berkas .env dapat diakses melalui properti process.env.
-const Jwt = require('@hapi/jwt');
+// mengimpor dotenv dan menjalankan konfigurasinya
+require('dotenv').config();
+
 const Hapi = require('@hapi/hapi');
+const Jwt = require('@hapi/jwt');
+
+// notes
 const notes = require('./api/notes');
+const NotesService = require('./services/postgres/NotesService');
+const NotesValidator = require('./validator/notes');
+
+// users
 const users = require('./api/users');
+const UsersService = require('./services/postgres/UsersService');
+const UsersValidator = require('./validator/users');
+
+// authentications
 const authentications = require('./api/authentications');
 const AuthenticationsService = require('./services/postgres/AuthenticationsService');
-const TokenManager = require('./tokenize/TokenManger');
-const NotesService = require('./services/postgres/NotesService');
-const UsersService = require('./services/postgres/UsersService');
-const NotesValidator = require('./validator/notes');
-const UsersValidator = require('./validator/users');
+const TokenManager = require('./tokenize/TokenManager');
 const AuthenticationsValidator = require('./validator/authentications');
 
 const init = async () => {
-  const authenticationsService = new AuthenticationsService();
-  const usersService = new UsersService();
   const notesService = new NotesService();
+  const usersService = new UsersService();
+  const authenticationsService = new AuthenticationsService();
+
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -26,13 +35,15 @@ const init = async () => {
     },
   });
 
+  // registrasi plugin eksternal
   await server.register([
     {
       plugin: Jwt,
-    }
+    },
   ]);
 
-  server.auth.strategy('notesapp_jwt', 'jwt',{
+  // mendefinisikan strategy autentikasi jwt
+  server.auth.strategy('notesapp_jwt', 'jwt', {
     keys: process.env.ACCESS_TOKEN_KEY,
     verify: {
       aud: false,
@@ -41,26 +52,26 @@ const init = async () => {
       maxAgeSec: process.env.ACCESS_TOKEN_AGE,
     },
     validate: (artifacts) => ({
-      isValide : true,
-      credential: {
-        id: artifacts.decode.payload.id,
+      isValid: true,
+      credentials: {
+        id: artifacts.decoded.payload.id,
       },
     }),
-  }); 
-  
+  });
+
   await server.register([
     {
       plugin: notes,
       options: {
         service: notesService,
-        validator: NotesValidator,  
+        validator: NotesValidator,
       },
     },
     {
       plugin: users,
       options: {
         service: usersService,
-        validator: UsersValidator,  
+        validator: UsersValidator,
       },
     },
     {
@@ -69,11 +80,10 @@ const init = async () => {
         authenticationsService,
         usersService,
         tokenManager: TokenManager,
-        validator: AuthenticationsValidator,  
+        validator: AuthenticationsValidator,
       },
-    }
-
-]);
+    },
+  ]);
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
